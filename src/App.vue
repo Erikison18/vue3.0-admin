ed22b75d9d774ae49609bdad707ee01cc797472c
曹强 <cq872286926@163.com>
2023-12-11 15:08:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+console.log(999)
+let aa = 11
+const str = '885pp'
+console.log(888)
+</script>
+
+<template>
+  <div class="box">
+    box
+    <h1>111</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: beige;
+
+  h1 {
+    color: aquamarine;
+  }
+}
+</style>

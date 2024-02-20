afa5329ec2bfa849b7d4d6a9419dd1608cc8c294
曹强 <cq872286926@163.com>
2024-02-20 17:42:39 +0800

    feat: trademark

--- src/api/product/trademark/type.ts
+++ src/api/product/trademark/type.ts
@@ -1,26 +1,27 @@
-export interface loginForm {
-  username: string
-  password: string
-}
-
 export interface ResponseData {
   code: number
   message: string
   ok: boolean
 }
 
-export interface loginResponseData extends ResponseData {
-  data: string
+//已有的品牌的ts数据类型
+export interface TradeMark {
+  id?: number
+  tmName: string
+  logoUrl: string
 }
 
-interface userInfo {
-  avatar: string
-  name: string
-  roles: string[]
-  buttons: string[]
-  routes: string[]
-}
+//包含全部品牌数据的ts类型
+export type Records = TradeMark[]
 
-export interface userResponseData extends ResponseData {
-  data: userInfo
+//获取的已有全部品牌的数据ts类型
+export interface TradeMarkResponseData extends ResponseData {
+  data: {
+    records: Records
+    total: number
+    size: number
+    current: number
+    searchCount: boolean
+    pages: number
+  }
 }
